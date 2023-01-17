39ab3aa429640767136958a60b16e0b620b79adb
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2023-01-17 13:18:38 +0200

    complete first version of the websocket chat

---
+++ src/main/java/com/example/websocketschat/WebSocketsChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketsChatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebSocketsChatApplication.class, args);
+  }
+
+}
